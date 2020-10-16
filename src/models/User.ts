cfb7ae1beb19a38e7ff9a86c8bf1c45153628cbf
edwinisnow <edwin04@gmail.com>
2020-10-16 22:19:30 +0100

    On change events

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,9 +2,11 @@ interface UserProps {
     name: string;
     age: number;
 }
+type Callback = () => void;
+
 export class User {
     constructor(private data: UserProps) { }
-
+    events: { [key: string]: Callback[] } = {}
     get(propName: string): (number | string) {
         return this.data[propName]
     }
@@ -12,4 +14,22 @@ export class User {
     set(update: UserProps): void {
         Object.assign(this.data, update)
     }
+
+    on(eventName: string, callback: Callback): void {
+        const handlers = this.events[eventName] || [];
+        handlers.push(callback)
+        this.events[eventName] = handlers;
+    }
+
+    trigger(eventName: string): void {
+        const handlers = this.events[eventName];
+        if (!handlers || handlers.length === 0) {
+            return;
+        }
+        handlers.forEach(callback => {
+            callback()
+        })
+    }
+
+
 }
