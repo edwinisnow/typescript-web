951030232b546e4870f39e17ab585944be938170
edwinisnow <edwin04@gmail.com>
2020-10-17 18:31:13 +0100

    Eventing

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,9 @@ import { User } from './models/User'
 // const user = new User({ id: 1 })
 const user = new User({ name: 'Gnishilda', age: 8 })
 // user.set({ name: 'Edwin - Updated', age: 40 })
-user.save();
+user.events.on('change', () => {
+    console.log('Change');
+
+})
+user.events.trigger('change')
 
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,36 +1,21 @@
 import axios, { AxiosResponse } from 'axios'
+import { Eventing } from './Eventing';
 interface UserProps {
     id?: number;
     name?: string;
     age?: number;
 }
-type Callback = () => void;
 
 export class User {
+    public events: Eventing = new Eventing();
     constructor(private data: UserProps) { }
-    events: { [key: string]: Callback[] } = {}
+
     get(propName: string): (number | string) {
         return this.data[propName]
     }
 
     set(update: UserProps): void {
         Object.assign(this.data, update)
-    }
-
-    on(eventName: string, callback: Callback): void {
-        const handlers = this.events[eventName] || [];
-        handlers.push(callback)
-        this.events[eventName] = handlers;
-    }
-
-    trigger(eventName: string): void {
-        const handlers = this.events[eventName];
-        if (!handlers || handlers.length === 0) {
-            return;
-        }
-        handlers.forEach(callback => {
-            callback()
-        })
     }
 
     fetch(): void {
@@ -43,10 +28,8 @@ export class User {
     save(): void {
         const id = this.get('id')
         if (id) {
-            // put
             axios.put(`http://localhost:3000/users/${id}`, this.data)
         } else {
-            // post
             axios.post(`http://localhost:3000/users/`, this.data)
         }
     }
